dd390750e6677b9eeb52384ae8939d392f336568
Jonathan Beliën <jbe@geo6.be>
2018-10-26 15:32:40 +0200

    Add PHPUnit tests

---
+++ Tests/AddokTest.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Geocoder package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Geocoder\Provider\Addok\Tests;
+
+use Geocoder\IntegrationTest\BaseTestCase;
+use Geocoder\Provider\Addok\Addok;
+use Geocoder\Query\GeocodeQuery;
+use Geocoder\Query\ReverseQuery;
+
+class AddokTest extends BaseTestCase
+{
+    protected function getCacheDir()
+    {
+        return __DIR__.'/.cached_responses';
+    }
+
+    /**
+     * @expectedException \Geocoder\Exception\UnsupportedOperation
+     * @expectedExceptionMessage The Addok provider does not support IP addresses, only street addresses.
+     */
+    public function testGeocodeWithLocalhostIPv4()
+    {
+        $provider = Addok::withBANServer($this->getMockedHttpClient(), 'Geocoder PHP/Addok Provider/Addok Test');
+        $provider->geocodeQuery(GeocodeQuery::create('127.0.0.1'));
+    }
+
+    /**
+     * @expectedException \Geocoder\Exception\UnsupportedOperation
+     * @expectedExceptionMessage The Addok provider does not support IP addresses, only street addresses.
+     */
+    public function testGeocodeWithLocalhostIPv6()
+    {
+        $provider = Addok::withBANServer($this->getMockedHttpClient(), 'Geocoder PHP/Addok Provider/Addok Test');
+        $provider->geocodeQuery(GeocodeQuery::create('::1'));
+    }
+
+    /**
+     * @expectedException \Geocoder\Exception\UnsupportedOperation
+     * @expectedExceptionMessage The Addok provider does not support IP addresses, only street addresses.
+     */
+    public function testGeocodeWithRealIPv6()
+    {
+        $provider = Addok::withBANServer($this->getMockedHttpClient(), 'Geocoder PHP/Addok Provider/Addok Test');
+        $provider->geocodeQuery(GeocodeQuery::create('::ffff:88.188.221.14'));
+    }
+
+    public function testReverseQuery()
+    {
+        $provider = Addok::withBANServer($this->getHttpClient(), 'Geocoder PHP/Addok Provider/Addok Test');
+        $results = $provider->reverseQuery(ReverseQuery::fromCoordinates(49.031407, 2.060204));
+
+        $this->assertInstanceOf('Geocoder\Model\AddressCollection', $results);
+        $this->assertCount(1, $results);
+
+        /** @var \Geocoder\Model\Address $result */
+        $result = $results->first();
+        $this->assertInstanceOf('\Geocoder\Model\Address', $result);
+        $this->assertEquals('6', $result->getStreetNumber());
+        $this->assertEquals('Quai de la Tourelle', $result->getStreetName());
+        $this->assertEquals('95000', $result->getPostalCode());
+        $this->assertEquals('Cergy', $result->getLocality());
+    }
+
+    public function testGeocodeQuery()
+    {
+        $provider = Addok::withBANServer($this->getHttpClient(), 'Geocoder PHP/Addok Provider/Addok Test');
+        $results = $provider->geocodeQuery(GeocodeQuery::create('6 quai de la tourelle cergy'));
+
+        $this->assertInstanceOf('Geocoder\Model\AddressCollection', $results);
+        $this->assertCount(1, $results);
+
+        /** @var \Geocoder\Model\Address $result */
+        $result = $results->first();
+        $this->assertInstanceOf('\Geocoder\Model\Address', $result);
+        $this->assertEquals(49.031407, $result->getCoordinates()->getLatitude(), '', 0.00001);
+        $this->assertEquals(2.060204, $result->getCoordinates()->getLongitude(), '', 0.00001);
+        $this->assertEquals('6', $result->getStreetNumber());
+        $this->assertEquals('Quai de la Tourelle', $result->getStreetName());
+        $this->assertEquals('95000', $result->getPostalCode());
+        $this->assertEquals('Cergy', $result->getLocality());
+    }
+}
---
+++ Tests/IntegrationTest.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * This file is part of the Geocoder package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license    MIT License
+ */
+
+namespace Geocoder\Provider\Addok\Tests;
+
+use Geocoder\IntegrationTest\ProviderIntegrationTest;
+use Geocoder\Provider\Addok\Addok;
+use Http\Client\HttpClient;
+
+class IntegrationTest extends ProviderIntegrationTest
+{
+    protected $testAddress = true;
+
+    protected $testReverse = true;
+
+    protected $testIpv4 = false;
+
+    protected $testIpv6 = false;
+
+    protected $skippedTests = [
+        'testGeocodeQuery' => 'BAN Server supports France only.',
+        'testReverseQuery' => 'BAN Server supports France only.',
+        'testReverseQueryWithNoResults' => 'Addok returns "debug" information for reverse geocoding on coordinates 0, 0. See https://github.com/addok/addok/issues/505'
+    ];
+
+    protected function createProvider(HttpClient $httpClient)
+    {
+        return Addok::withBANServer($httpClient, 'Geocoder PHP/Addok Provider/Integration Test');
+    }
+
+    protected function getCacheDir()
+    {
+        return __DIR__.'/.cached_responses';
+    }
+
+    protected function getApiKey()
+    {
+    }
+}
